cfc1bf9802b5beced20e0748fab2f73fb1fe5ef9
Saloni-1415 <salonisatav85@gmail.com>
2026-07-06 19:42:31 +0530

    Java Basic Boilerplate code

---
+++ JavaBasics.java
@@ -0,0 +1,14 @@
+//name of class and file should be same in java.
+public class JavaBasics {
+    public static void main(String args[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+
+}
+//THIS IS A BOILER PLATE CODE 
+
+
+
