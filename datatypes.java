b8c097dba38aa3b58870f74d5dcd618505317482
Saloni-1415 <salonisatav85@gmail.com>
2026-07-07 22:52:12 +0530

    Add Java data types

---
+++ datatypes.java
@@ -0,0 +1,25 @@
+// In Java, data types define what kind of value a variable can store.
+
+public class datatypes {
+    public static void main(String[] args) {
+        byte age = 20;
+        short year = 2026;
+        int marks = 95;
+        long population = 8000000000L;
+
+        float pi = 3.14f;
+        double salary = 45000.75;
+
+        char grade = 'A';
+        boolean isPassed = true;
+
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(marks);
+        System.out.println(population);
+        System.out.println(pi);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(isPassed);
+    }
+}
