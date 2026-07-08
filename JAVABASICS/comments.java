f1f78e679300485c50351d71f3778c5a52772924
Saloni-1415 <salonisatav85@gmail.com>
2026-07-08 08:41:13 +0530

    Organize Java basics files into JavaBasics folder

---
+++ JAVABASICS/comments.java
@@ -0,0 +1,18 @@
+public class comments {
+    public static void main(String[] args) {
+
+        // Single-line comment
+        int age = 20;
+
+        /*
+         Multi-line comment
+         Displaying age
+        */
+        System.out.println(age);
+
+    }
+}
+
+/* Comments are lines of text that are ignored by the Java compiler. 
+ They are used to explain code, improve readability, and make programs 
+ easier to understand and maintain.*/
