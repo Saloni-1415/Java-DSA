9b3371bb29caa5cb765879286d567adc4231617e
Saloni-1415 <salonisatav85@gmail.com>
2026-07-14 21:17:29 +0530

    Add type conversion program

---
+++ JAVABASICS/typeconversion.java
@@ -0,0 +1,9 @@
+public class typeconversion{
+    public static void main(String[] args) {
+        int num = 50;
+        double d = num;   // int → double
+
+        System.out.println(num);
+        System.out.println(d);
+    }
+}
