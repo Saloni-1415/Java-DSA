a3e175d8389c8c8289460b7a197b8fc3890ffd1c
Saloni-1415 <salonisatav85@gmail.com>
2026-07-15 20:18:10 +0530

    Add switch-case practice programs

---
+++ CONTROLSTATEMENTS/SwitchCase.java
@@ -0,0 +1,17 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int number = 2 ;
+
+        switch(number){
+            case 1 : System.out.println("Samosa ");
+            break;
+            case 2 : System.out.println("Burger ");
+            break;
+            case 3 : System.out.println("Pizzza ");
+            break;
+            default: System.out.println("Chineese ");
+
+        }
+        
+    }
+}
