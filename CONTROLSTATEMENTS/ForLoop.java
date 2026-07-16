170557066a3b2f766cb5d86319eaa7f9a95e0810
Saloni-1415 <salonisatav85@gmail.com>
2026-07-16 12:12:22 +0530

    For Loop Examples

---
+++ CONTROLSTATEMENTS/ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        
+        // Print numbers from 1 to 5
+        for (int number = 1; number <= 5; number++) {
+            System.out.println(number);
+
+        System.out.println("----------------------------------------------");
+
+        // only even numbers from 1 to 10
+        for (int i = 2;i<=5;i++;){
+            System.out.println(i);
+        }
+        }
+    }
+}
